bd4cb450b620cc8df1434ede139abd3242924784
Joni <hangjoni@gmail.com>
2023-11-03 16:08:35 +0800

    some styling to summary

--- src/components/ToDoSummary.vue
+++ src/components/ToDoSummary.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-for="cat in itemsSummary" :key="cat.id">
-    <div>
-      <h3>{{ cat.label }}</h3>
-      <h1>{{ cat.count }}</h1>
+  <section class="flex flex-col md:flex-row md:px-12">
+    <div v-for="cat in itemsSummary" :key="cat.id" class="flex-1 px-8 py-2">
+      <h3 class="text-lg">{{ cat.label }}</h3>
+      <h1 class="text-xl">{{ cat.count }}</h1>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup>
