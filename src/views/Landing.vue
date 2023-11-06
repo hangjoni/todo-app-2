d8d30af6ff44d45090c4c1e2536e06ea81fff65c
Joni <hangjoni@gmail.com>
2023-11-06 13:02:48 +0800

    added a modal for create new project

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="landing">Landing</div>
+  <div class="landing">
+    <h3>This is the home page</h3>
+    <button class="bg-pink-200" @click="showCreateProjectModal">Create new project</button>
+    <Modal name="create-new-project" title="Create a new project"> </Modal>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { inject } from 'vue'
+import Modal from '../plugins/modals/Modal.vue'
+
+const $modals = inject('$modals')
+
+const showCreateProjectModal = () => {
+  $modals.show('create-new-project')
+}
+</script>
 
 <style scoped>
 .landing {
